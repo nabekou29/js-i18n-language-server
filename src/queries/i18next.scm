e78387d8532f45ad69624c391cf6435fd2aac7bd
nabekou29 <nabekou0729@gmail.com>
2025-02-09 17:00:26 +0900

    add: copy queries

---
+++ src/queries/i18next.scm
@@ -0,0 +1,47 @@
+;; Call function to get `t`
+(variable_declarator
+  name: (identifier) @i18n.t_func_name
+  value:
+    (call_expression
+      function: [
+        (identifier)
+        (member_expression)
+      ] @get_fixed_t_func (#match? @get_fixed_t_func "getFixedT$")
+      ;; 1: lang, 2: ns, 3: keyPrefix
+      arguments: (arguments
+        (          [
+            (string (string_fragment))
+            (undefined)
+            (null)
+          ]
+        )?
+        (
+          [
+            (string (string_fragment) @i18n.namespace)
+            (undefined)
+            (null)
+          ]
+        )?
+        (
+          [
+            (string (string_fragment) @i18n.key_prefix)
+            (undefined)
+            (null)
+          ]
+        )?
+      )
+    )
+) @i18n.get_t
+
+;; Call `t` function
+(call_expression
+  function: [
+    (identifier)
+    (member_expression)
+  ] @i18n.t_func_name
+    arguments: (arguments
+      (string
+        (string_fragment) @i18n.key
+      ) @i18n.key_arg
+    )
+) @i18n.call_t
---
+++ src/queries/next-intl.scm
@@ -0,0 +1,27 @@
+;; Call function to get `t`
+(variable_declarator
+  name: (identifier) @i18n.t_func_name
+  value:
+    (call_expression
+      function: (identifier) @use_translations (#eq? @use_translations "useTranslations")
+      arguments: (arguments
+        [
+          (string (string_fragment) @i18n.key_prefix)
+          (undefined)
+        ]?
+      )
+    )
+) @i18n.get_t
+
+;; Call `t` function
+(call_expression
+  function: [
+    (identifier)
+    (member_expression)
+  ] @i18n.t_func_name
+  arguments: (arguments
+    (string
+      (string_fragment) @i18n.key
+    ) @i18n.key_arg
+  )
+) @i18n.call_t
